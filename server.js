ec85c9d890ce0454c76f5111db9bcbfbf6b0bf68
asia <asia_jcrew96@yahoo.com>
2022-04-02 12:10:36 -0400

    added CRUD functionality to server.js

--- server.js
+++ server.js
@@ -1,10 +1,55 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connect to SQL database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Janay0810!$',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// Display all current candidates
+// db.query(`SELECT * FROM candidates`, (err, rows)=> {
+//     console.log(rows);
+// });
+
+// Select a specific candidate
+// db.query(`SELECT * FROM candidates WHERE id = 8`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(row);
+// });
+
+// Delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = 2`, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     }
+//     console.log(result);
+// });
+
+// Create a new candidate
+const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+VALUES (?,?,?,?)`;
+const params = [2, 'Virginia', 'Wolf', 1];
+
+db.query(sql, params, (err, result) => {
+    if (err) {
+        console.log(err);
+    }
+    console.log(result);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
